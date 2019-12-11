3c5ac09f81e3f5797bbd30bd36f8884e0b04fe8e
Pierre Marchand <pierremarc07@gmail.com>
2019-12-11 07:19:12 +0100

    sql typo

--- shell/src/sql/select_ending_task.sql
+++ shell/src/sql/select_ending_task.sql
@@ -1,10 +1,15 @@
-SELECT 
-    d.id, d.username, d.end_time
-FROM 
-    command_do as d
-    LEFT JOIN notif as n 
-        ON d.id = n.task_id 
+SELECT
+  d.id,
+  d.username,
+  d.end_time
+FROM command_do as d
+LEFT JOIN notif as n ON d.id = n.task_id
 WHERE
-    (n.id IS NULL OR n.end_time <> d.end_time)
-    AND (d.start_time + ((d.end_time - d.start_time) * 0.9)) < :now  ;
-    AND :now < d.end_time 
+  (
+    n.id IS NULL
+    OR n.end_time <> d.end_time
+  )
+  AND (
+    d.start_time + ((d.end_time - d.start_time) * 0.9)
+  ) < :now
+  AND :now < d.end_time;
