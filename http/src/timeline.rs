a7b09c7bd67cc04d26dc639da21632073c984a65
Pierre Marchand <pierre-m@atelier-cartographique.be>
2020-12-12 12:10:29 +0100

    timeline page

--- http/src/timeline.rs
+++ http/src/timeline.rs
@@ -1,9 +1,10 @@
 use html::{body, div, h1, h2, head, html, span, style, with_doctype, Element, Empty};
 use shell::{
-    store::{AggregatedTaskRecord, ProjectRecord},
-    util::{date_time_from_st, human_duration, human_st},
+    store::{AggregatedTaskRecord, ProjectRecord, StoreError},
+    util::date_time_from_st,
 };
 use std::{
+    cmp::Ordering,
     convert::Infallible,
     time::{Duration, SystemTime},
 };
@@ -11,7 +12,31 @@ use warp::Filter;
 
 use crate::{with_store, ArcStore};
 
-type TimelineProject = (ProjectRecord, Option<std::time::Duration>);
+type TimelineProject = (ProjectRecord, std::time::Duration);
+
+fn cmp_by_deadline(a: &TimelineProject, b: &TimelineProject) -> Ordering {
+    if a.0.end_time.is_some() & b.0.end_time.is_none() {
+        return Ordering::Less;
+    }
+    if a.0.end_time.is_none() & b.0.end_time.is_some() {
+        return Ordering::Greater;
+    }
+    let now = std::time::SystemTime::now();
+    let a_remaining =
+        a.0.end_time
+            .and_then(|t| now.duration_since(t).ok())
+            .unwrap_or(Duration::new(u64::MAX, 1_000_000_000 - 1));
+    let b_remaining =
+        b.0.end_time
+            .and_then(|t| now.duration_since(t).ok())
+            .unwrap_or(Duration::new(u64::MAX, 1_000_000_000 - 1));
+
+    if a_remaining == b_remaining {
+        a.0.id.cmp(&b.0.id)
+    } else {
+        a_remaining.cmp(&b_remaining)
+    }
+}
 
 fn format_date(t: &SystemTime) -> String {
     let dt = date_time_from_st(t);
@@ -45,9 +70,9 @@ fn format_hour(d: Duration) -> String {
     format!("{}h", to_hour(d))
 }
 
-fn make_gauge(provision: Duration, done: Duration) -> Element {
-    let p = provision.as_secs();
-    let d = done.as_secs();
+fn make_gauge(provision: &Duration, done: &Duration) -> Element {
+    let p = provision.as_secs() + 1;
+    let d = done.as_secs() + 1;
     let (done_percent, avail_percent, over_percent) = if d > p {
         (p * 100 / d, 0, (d - p) * 100 / d)
     } else {
@@ -72,18 +97,61 @@ fn make_full(
     name: &str,
     _start_time: &SystemTime,
     end: &SystemTime,
-    provision: Duration,
-    done: Duration,
+    provision: &Duration,
+    done: &Duration,
 ) -> Element {
     div(vec![
         make_gauge(provision, done),
         div(vec![
             h2(name),
             kv("Deadline:", &format_date(end)),
-            remaining(provision, done),
-            kv("Provisioned:", &format_hour(provision)),
-            kv("Done:", &format_hour(done)),
+            remaining(*provision, *done),
+            kv("Provisioned:", &format_hour(*provision)),
+            kv("Done:", &format_hour(*done)),
         ]),
+    ])
+    .class("project-wrapper")
+}
+
+fn make_with_provision(
+    name: &str,
+    _start_time: &SystemTime,
+    provision: &Duration,
+    done: &Duration,
+) -> Element {
+    div(vec![
+        make_gauge(provision, done),
+        div(vec![
+            h2(name),
+            remaining(*provision, *done),
+            kv("Provisioned:", &format_hour(*provision)),
+            kv("Done:", &format_hour(*done)),
+        ]),
+    ])
+    .class("project-wrapper")
+}
+
+fn make_with_end(
+    name: &str,
+    _start_time: &SystemTime,
+    end: &SystemTime,
+    done: &Duration,
+) -> Element {
+    div(vec![
+        make_gauge(done, done),
+        div(vec![
+            h2(name),
+            kv("Deadline:", &format_date(end)),
+            kv("Done:", &format_hour(*done)),
+        ]),
+    ])
+    .class("project-wrapper")
+}
+
+fn make_bare(name: &str, _start_time: &SystemTime, done: &Duration) -> Element {
+    div(vec![
+        make_gauge(done, done),
+        div(vec![h2(name), kv("Done:", &format_hour(*done))]),
     ])
     .class("project-wrapper")
 }
@@ -98,23 +166,32 @@ fn get_done(tasks: Vec<AggregatedTaskRecord>) -> std::time::Duration {
         })
 }
 
-async fn timeline_handler(s: ArcStore) -> Result<impl warp::Reply, Infallible> {
+fn get_projects(s: ArcStore) -> Result<Vec<TimelineProject>, StoreError> {
     let store = s.lock().expect("hmmm");
     let projects = store.select_all_project_info().map(|rows| {
-        let projects: Vec<TimelineProject> = rows
+        let mut projects: Vec<TimelineProject> = rows
             .iter()
-            .map(|record| {
-                let tasks = store.select_project(record.name.clone());
-                let done = tasks.map(get_done);
-                println!(">> {}: {:?}", record.name.clone(), done);
-                (record.clone(), done.ok())
+            .filter_map(|record| {
+                if record.completed.is_some() {
+                    None
+                } else {
+                    let tasks = store.select_project(record.name.clone());
+                    let done = tasks.map(get_done);
+                    Some((record.clone(), done.unwrap_or(Duration::from_secs(0))))
+                }
             })
             .collect();
+        projects.sort_by(cmp_by_deadline);
         projects
     });
+
+    projects
+}
+
+async fn timeline_handler(s: ArcStore) -> Result<impl warp::Reply, Infallible> {
     let css = style(String::from(include_str!("timeline.css"))).set("type", "text/css");
 
-    match projects {
+    match get_projects(s) {
         Err(_) => Ok(warp::reply::html(with_doctype(html(vec![
             head(css),
             body(vec![div("error: no projects found")]),
@@ -122,47 +199,15 @@ async fn timeline_handler(s: ArcStore) -> Result<impl warp::Reply, Infallible> {
         Ok(projects) => {
             let elements: Vec<Element> = projects
                 .iter()
-                .map(|(p, d)| match (p.end_time, p.provision, *d) {
-                    (Some(end), Some(provision), Some(done)) => {
-                        make_full(&p.name, &p.start_time, &end, provision, done)
+                .map(|(p, done)| match (p.end_time, p.provision) {
+                    (Some(end), Some(provision)) => {
+                        make_full(&p.name, &p.start_time, &end, &provision, done)
                     }
-                    (None, Some(provision), Some(done)) => div(vec![
-                        h1(&p.name),
-                        div(format!("started: {}", human_st(&p.start_time))),
-                        div(format!("provision: {}", human_duration(provision))),
-                        div(format!("done: {}", human_duration(done))),
-                    ]),
-                    (None, None, Some(done)) => div(vec![
-                        h1(&p.name),
-                        div(format!("started: {}", human_st(&p.start_time))),
-                        div(format!("done: {}", human_duration(done))),
-                    ]),
-                    (None, None, None) => div(vec![
-                        h1(&p.name),
-                        div(format!("started: {}", human_st(&p.start_time))),
-                    ]),
-                    (Some(end), None, None) => div(vec![
-                        h1(&p.name),
-                        div(format!("started: {}", human_st(&p.start_time))),
-                        div(format!("due date: {}", human_st(&end))),
-                    ]),
-                    (Some(end), Some(provision), None) => div(vec![
-                        h1(&p.name),
-                        div(format!("started: {}", human_st(&p.start_time))),
-                        div(format!("due date: {}", human_st(&end))),
-                        div(format!("provision: {}", human_duration(provision))),
-                    ]),
-                    (Some(end), None, Some(done)) => div(vec![
-                        h1(&p.name),
-                        div(format!("started: {}", human_st(&p.start_time))),
-                        div(format!("due date: {}", human_st(&end))),
-                        div(format!("done: {}", human_duration(done))),
-                    ]),
-                    (None, Some(provision), None) => div(vec![
-                        h1(&p.name),
-                        div(format!("started: {}", human_st(&p.start_time))),
-                        div(format!("provision: {}", human_duration(provision))),
-                    ]),
+                    (None, Some(provision)) => {
+                        make_with_provision(&p.name, &p.start_time, &provision, done)
+                    }
+                    (Some(end), None) => make_with_end(&p.name, &p.start_time, &end, done),
+                    (None, None) => make_bare(&p.name, &p.start_time, done),
                 })
                 .collect();
 
